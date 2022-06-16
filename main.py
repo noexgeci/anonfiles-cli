5a8ce286a9abf0b75d75b30a58a7066c60d66c3f
noexskid <86523746+noexskid@users.noreply.github.com>
2022-06-16 12:14:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ Drag & Drop the file here and press Enter to upload
 filename = input(f"{Fore.GREEN}? {Fore.RESET}File {Fore.GREEN}→ {Fore.LIGHTGREEN_EX}")
 
 if filename == "":
-	print(f'{Fore.RED}! {Fore.RESET}No file specified')
-	os.system('pause >nul')
-	os.system('exit')
+    print(f'{Fore.RED}! {Fore.RESET}No file specified')
+    os.system('pause >nul')
+    os.system('exit')
 
 files = {'file': (open(filename, 'rb'))}
 
@@ -26,12 +26,12 @@ r = requests.post(url, files=files)
 response = json.loads(r.text)
 
 if response['status']:
-	url0 = response['data']['file']['url']['short']
-	print(f'{Fore.CYAN}! {Fore.RESET}File uploaded successfully {Fore.CYAN}→ {Fore.LIGHTCYAN_EX}{url0}')
-	os.system(f'start {url0}')
-	os.system('pause >nul')
+    url0 = response['data']['file']['url']['short']
+    print(f'{Fore.CYAN}! {Fore.RESET}File uploaded successfully {Fore.CYAN}→ {Fore.LIGHTCYAN_EX}{url0}')
+    os.system(f'start {url0}')
+    os.system('pause >nul')
 else:
-	message = resp['error']['message']
-	errtype = resp['error']['type']
+    message = resp['error']['message']
+    errtype = resp['error']['type']
 	print(f'{Fore.RED}! {Fore.RESET}Error {Fore.RED}→ {Fore.LIGHTRED_EX}{message} {Fore.RED}| {Fore.LIGHTRED_EX}{errtype}')
 	os.system('pause >nul')
